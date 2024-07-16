974832e578de6dfc7c9f45beaa41c856d721745a
ManikantaReddyN <manikantareddy@csu.fullerton.edu>
2024-07-16 15:15:05 -0700

    chore: calculate points for receipt when processing the receipt request to overcome redundant calculation

--- src/main/java/com/fetch/rewards/controllers/ReceiptProcessController.java
+++ src/main/java/com/fetch/rewards/controllers/ReceiptProcessController.java
@@ -1,17 +1,25 @@
 package com.fetch.rewards.controllers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fetch.rewards.components.schemas.Receipt;
+import com.fetch.rewards.repository.RequestBodyValidator;
 import com.fetch.rewards.service.ReceiptService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/receipts")
 public class ReceiptProcessController {
     private final ReceiptService receiptService;
+    private final RequestBodyValidator requestBodyValidator;
 
-    public ReceiptProcessController(ReceiptService receiptService) {
+    public ReceiptProcessController(ReceiptService receiptService, RequestBodyValidator requestBodyValidator) {
         this.receiptService = receiptService;
+        this.requestBodyValidator = requestBodyValidator;
     }
 
     /*
@@ -20,16 +28,34 @@ public class ReceiptProcessController {
      */
 
     @PostMapping(value="/process")
-    public ResponseEntity<String> processReceipts(@RequestBody Receipt receipt) {
-        String id = receiptService.processReceipt(receipt);
-        return ResponseEntity.ok(id);
+    public ResponseEntity<Object> processReceipts(@RequestBody String requestBody) {
+
+        if(requestBodyValidator.isValidJson(requestBody, Receipt.class)) {
+            Receipt receipt;
+            try {
+                receipt = new ObjectMapper().readValue(requestBody, Receipt.class);
+                String id = receiptService.processReceipt(receipt);
+                Map<String,String> response = new HashMap<>();
+                response.put("id", id);
+                return ResponseEntity.status(HttpStatus.OK).body(response);
+            } catch (Exception e) {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("The receipt is invalid");
+            }
+        }
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("The receipt is invalid");
     }
 
     @GetMapping(value="/{id}/points")
-    public ResponseEntity<Integer> getPoints(@PathVariable ("id") String id) {
-        int points = receiptService.calculatePoints(id);
+    public ResponseEntity<?> getPoints(@PathVariable ("id") String id) {
+        Integer points = receiptService.getPoints(id);
+        if(points == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No receipt found for that id");
+        }
+        Map<String,Integer> response = new HashMap<>();
+        response.put("points", points);
 
-        return ResponseEntity.ok(points);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
 
     }
 }
